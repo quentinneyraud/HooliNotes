d59739b7e75a3360c04abc313fce5d9ab900e136
Quentin Neyraud <quentin.neyraud@gmail.com>
2016-02-05 14:58:33 +0100

    add list notes activity

---
+++ app/src/main/java/fr/quentinneyraud/www/hoolinotes/ListNotesActivity.java
@@ -0,0 +1,13 @@
+package fr.quentinneyraud.www.hoolinotes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListNotesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_notes);
+    }
+}
