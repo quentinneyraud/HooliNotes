82e3acfd8c345a3658d0d09701e37c3e03954437
Quentin Neyraud <quentin.neyraud@gmail.com>
2016-02-07 23:41:53 +0100

    set up list fragment to use note adapter

--- app/src/main/java/fr/quentinneyraud/www/hoolinotes/NotesListFragment.java
+++ app/src/main/java/fr/quentinneyraud/www/hoolinotes/NotesListFragment.java
@@ -2,12 +2,21 @@ package fr.quentinneyraud.www.hoolinotes;
 
 
 import android.location.Location;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import fr.quentinneyraud.www.hoolinotes.Notes.Note;
+import fr.quentinneyraud.www.hoolinotes.Notes.NoteAdapter;
 
 
 /**
@@ -17,10 +26,9 @@ public class NotesListFragment extends Fragment {
 
     private static final String TAG = "NOTES LIST FRAGMENT ===";
 
-    public NotesListFragment() {
-        // Required empty public constructor
-        Log.d(TAG, "constructor");
-    }
+    private NoteAdapter noteAdapter;
+
+    public NotesListFragment() {}
 
     public void setLocation(Location location){
         Log.d(TAG, "position");
@@ -29,9 +37,35 @@ public class NotesListFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        Log.d(TAG, "create view");
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_notes_list, container, false);
+        View view = inflater.inflate(R.layout.fragment_notes_list, container, false);
+
+        // Get recycler view
+        RecyclerView rcView = (RecyclerView) view.findViewById(R.id.fragment_notes_recycler_view);
+        rcView.setLayoutManager(new LinearLayoutManager(view.getContext()));
+
+        // Create adapter
+        noteAdapter = new NoteAdapter(new ArrayList<Note>());
+
+        // Set listener
+        noteAdapter.setNoteClickListener(new NoteAdapter.NoteClickListener() {
+            @Override
+            public void onClick(View v, long id) {
+                Log.d(TAG, "Click on " + String.valueOf(id));
+            }
+        });
+
+        rcView.setAdapter(noteAdapter);
+
+        this.loadDatas();
+
+        return view;
+    }
+
+    private void loadDatas(){
+        // TODO : get notes, pass them to adapter & notify change
+        // noteAdapter.addNotes(listNotes);
+        // noteAdapter.notifyDataSetChanged();
     }
 
 }
