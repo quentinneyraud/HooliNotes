196f013c3a1d5ec269b22dca0594a438c90808c6
Quentin Neyraud <quentin.neyraud@gmail.com>
2016-02-07 11:22:18 +0100

    add list notes activity

--- app/src/main/java/fr/quentinneyraud/www/hoolinotes/SplashScreenActivity.java
+++ app/src/main/java/fr/quentinneyraud/www/hoolinotes/SplashScreenActivity.java
@@ -24,7 +24,7 @@ public class SplashScreenActivity extends AppCompatActivity {
 
             @Override
             public void run() {
-                Intent i = new Intent(SplashScreenActivity.this, ListNotesActivity.class);
+                Intent i = new Intent(SplashScreenActivity.this, NotesActivity.class);
                 startActivity(i);
 
                 finish();
