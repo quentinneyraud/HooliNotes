397f55614855e9e2eb9070b7f7f2d462cc18ec04
Quentin Neyraud <quentin.neyraud@gmail.com>
2016-02-09 00:50:03 +0100

    add User class

---
+++ app/src/main/java/fr/quentinneyraud/www/hoolinotes/User/User.java
@@ -0,0 +1,46 @@
+package fr.quentinneyraud.www.hoolinotes.User;
+
+import android.content.Context;
+
+import com.firebase.client.ChildEventListener;
+import com.firebase.client.Firebase;
+
+import fr.quentinneyraud.www.hoolinotes.MyApp;
+import fr.quentinneyraud.www.hoolinotes.R;
+
+/**
+ * Created by quentin on 08/02/16.
+ */
+public class User {
+
+    private String uId;
+    private Firebase userRef;
+
+    public String getuId() {
+        return uId;
+    }
+
+    public void setuId(String uId) {
+        this.uId = uId;
+    }
+
+    public User(){
+
+    }
+
+    public User(String uId){
+
+        // Get context & create reference to user
+        Context appContext = MyApp.getContext();
+        String url = appContext.getResources().getString(R.string.firebase_base) + "/users/" + uId;
+        userRef = new Firebase(url);
+
+        setuId(uId);
+    }
+
+    public void getNotes(ChildEventListener cel){
+        userRef.child("notes").addChildEventListener(cel);
+    }
+
+
+}
