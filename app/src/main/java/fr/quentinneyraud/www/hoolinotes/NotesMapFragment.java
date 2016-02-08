1ed4d265db83da3cc0817cc5462c0a9177d6271b
Quentin Neyraud <quentin.neyraud@gmail.com>
2016-02-09 00:56:53 +0100

    remove Log

--- app/src/main/java/fr/quentinneyraud/www/hoolinotes/NotesMapFragment.java
+++ app/src/main/java/fr/quentinneyraud/www/hoolinotes/NotesMapFragment.java
@@ -4,7 +4,6 @@ package fr.quentinneyraud.www.hoolinotes;
 import android.location.Location;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,18 +18,14 @@ public class NotesMapFragment extends Fragment {
 
     public NotesMapFragment() {
         // Required empty public constructor
-        Log.d(TAG, "constructor");
     }
 
     public void setLocation(Location location){
-        Log.d(TAG, "position");
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
-        Log.d(TAG, "create view");
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_notes_map, container, false);
     }
